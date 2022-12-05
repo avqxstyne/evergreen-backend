33c1a9496dcc16f70d44e8cd9ad6afd595dd2835
August <augustwiesner16@gmail.com>
2022-12-05 10:57:53 -0600

    cors issue fixed. unsaved changes loaded

--- src/main/java/com/augustyne/inputApp/controller/FilesController.java
+++ src/main/java/com/augustyne/inputApp/controller/FilesController.java
@@ -4,7 +4,9 @@ import com.augustyne.inputApp.model.Files;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,10 +22,12 @@ public class FilesController {
     @Autowired
     private FilesService filesService;
 
+    @CrossOrigin
     @PostMapping("/add")
     public String add(@RequestBody Files files) {
         filesService.saveFiles(files);
-        return "File has been added";
+        
+        return "File added";
     }
 
     @GetMapping("/getAll")
